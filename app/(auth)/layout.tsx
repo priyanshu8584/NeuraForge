0e76b08a6c034b1a8beb83b445055d8a622b5982
Priyanshu <singh.priyanshu.1024@gmail.com>
2024-09-20 12:40:59 +0530

    good commit

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-export const layout = ({children}:{children:React.ReactNode}) => {
+const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <main className='auth'>layout
-    {children}
-    </main>
+    <main className="auth">{children}</main>
   )
 }
+
+export default Layout
